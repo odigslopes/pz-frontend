b7539a6dd4ffd4ff14f6fa93d19a5185c7dbed18
Diego Lopes <diegolopes.vieira@hotmail.com>
2024-01-23 21:49:26 -0300

    feat(color-mode): enhance button with icon and styles
    Improved the color mode toggle button by incorporating a moon icon for better visual comprehension and updated the styling for a more engaging user interface. The button now has specific margin and padding, a fresh background color, and text styling for improved visuals and accessibility.

--- src/components/color-mode/color-mode.tsx
+++ src/components/color-mode/color-mode.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { Button, useColorMode } from '@chakra-ui/react';
+import { Moon } from 'lucide-react';
 
 export function ColorMode() {
   const { toggleColorMode } = useColorMode();
-  return <Button onClick={toggleColorMode}>Mude de cor</Button>;
+  return (
+    <Button margin={6} padding={4} onClick={toggleColorMode} bg="#3AD5F8" color="white" size="sm">
+      <Moon size={20} />
+    </Button>
+  );
 }
