9d6a0650838730f320b1e183468b870d06c8874d
Diego Lopes <diegolopes.vieira@hotmail.com>
2024-01-24 04:36:09 -0300

    feat: add header and footer components
    Introduced new header and footer components to the application using Chakra UI for consistent styling and layout. The header includes navigation links and a color mode toggle, while the footer provides basic branding. This enhances user navigation and establishes a foundation for a cohesive UI across the application.

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import { Heading, HStack } from '@chakra-ui/react';
+
+export function Footer() {
+  return (
+    <HStack bg="#0D0221" justify="center" padding={6}>
+      <Heading>eu sou footer</Heading>
+    </HStack>
+  );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,42 @@
+import { Box, HStack, Link, Spacer } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+import { ColorMode } from '../color-mode';
+
+export function Header() {
+  return (
+    <HStack align="center" position="sticky" top={0} bg="#0D0221" zIndex="sticky" justify="space-between" height="80px">
+      <HStack padding={2} margin="auto" spacing={2}>
+        <Link
+          as={NextLink}
+          href="/"
+          padding={2}
+          borderWidth="1px"
+          borderRadius={10}
+          borderColor="gray.400"
+          fontSize="xl"
+          color="gray.400"
+          fontWeight="bold"
+        >
+          Mods
+        </Link>
+        <Link
+          as={NextLink}
+          href="/server"
+          padding={2}
+          borderWidth="1px"
+          borderRadius={10}
+          borderColor="gray.400"
+          fontSize="xl"
+          color="gray.400"
+          fontWeight="bold"
+        >
+          Servidor
+        </Link>
+      </HStack>
+      <Box position="absolute" right={0}>
+        <ColorMode />
+      </Box>
+    </HStack>
+  );
+}
