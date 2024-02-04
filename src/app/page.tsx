9466f8831a235153f639e86f53944414a20e43f3
Felipe Feliciano <fehzl@live.com>
2024-02-04 04:29:37 -0300

    refactor(app): streamline data fetching and update UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import { Heading, HStack, IconButton, Link, SimpleGrid, Text, VStack } from '@chakra-ui/react';
-import { ExternalLink } from 'lucide-react';
-import Image from 'next/image';
+import { Heading, HStack, VStack } from '@chakra-ui/react';
 
 import { Form } from '@/components/form';
+import { Mods } from '@/components/mods';
+import { getSteamData } from '@/lib/api-service';
 import { IMod } from '@/types/IMod';
 
-const getData = async () => {
-  const data = await fetch('https://dolphin-app-u66b7.ondigitalocean.app/steam', {
-    method: 'GET',
-    next: {
-      tags: ['mods'],
-    },
-  });
-  return data.json();
-};
-
 export default async function Home() {
-  const data: IMod[] = await getData();
+  const data: IMod[] = await getSteamData();
+
   return (
     <VStack padding={6}>
       <Heading>Consulta mods por ID</Heading>
@@ -25,38 +16,7 @@ export default async function Home() {
           <Form />
         </HStack>
       </HStack>
-      <SimpleGrid columns={[1, 2, 3, 4]} spacing={4} alignItems="flex-start" textAlign="justify">
-        {data.map((mod) => (
-          <VStack
-            key={mod.id}
-            height="100%"
-            spacing={4}
-            padding={4}
-            borderWidth="2px"
-            borderColor="gray.400"
-            borderRadius={8}
-          >
-            <Image src={mod.imageUrl} width={120} height={120} alt="image avatar" />
-            <HStack>
-              <Heading as="h1" size="sm" textTransform="uppercase">
-                {mod.title}
-              </Heading>
-              <IconButton
-                color="#3AD5F8"
-                as={Link}
-                variant="ghost"
-                aria-label="..."
-                href={mod.workshopUrl}
-                target="_blank"
-                icon={<ExternalLink />}
-              />
-            </HStack>
-            <Text flexGrow={1} noOfLines={3} fontSize="sm">
-              {mod.description}
-            </Text>
-          </VStack>
-        ))}
-      </SimpleGrid>
+      <Mods mods={data} preview />
     </VStack>
   );
 }
